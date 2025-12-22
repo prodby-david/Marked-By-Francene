518c989ceceb6f952b42703a1e9f778bd2c9989e
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-12-22 11:17:40 +0800

    feat(signup): add loader state

--- src/features/auth/signup/hooks/useSignUp.ts
+++ src/features/auth/signup/hooks/useSignUp.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from "react";
 import { SignUpType } from "../types/SignUpType";
 import { SignUpClientServices } from "../services/signup.client";
@@ -5,10 +7,11 @@ import { SignUpClientServices } from "../services/signup.client";
 
 export function useSignUp(){
 
-    const [data, setData] = useState<Omit<SignUpType, "confirmPassword">| null>(null);
+    const [ data, setData ] = useState<Omit<SignUpType, "confirmPassword">| null>(null);
+    const [ loading, setLoading ] = useState<boolean>(false);
 
     async function signUp(data: Omit<SignUpType, "id" | "createdAt" | "confirmPassword">) : Promise<Omit<SignUpType, "confirmPassword">> {
-
+        setLoading(true);
         try {
             const res = await SignUpClientServices.signUp(data);    
             setData(res);
@@ -16,9 +19,12 @@ export function useSignUp(){
         } catch (err) {
             throw err;
         }
+        finally {
+            setLoading(false);
+        }
         
     }
 
-    return { signUp, data };
+    return { signUp, data, loading};
 
 }
