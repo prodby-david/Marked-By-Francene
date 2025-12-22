3fe6df6b0dbb0c5532332481e160d3ab9ac4c5c1
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-12-22 11:18:59 +0800

    fix: remove unused export

--- src/features/auth/signup/index.ts
+++ src/features/auth/signup/index.ts
@@ -1,6 +1,5 @@
 export * from './components/SignUpForm';
 export * from './types/SignUpType';
 export * from './hooks/useSignUp';
-export * from './hooks/useSignUpForm'
 export * from './services/signup.client';
 export * from './validations/signUpSchema';
