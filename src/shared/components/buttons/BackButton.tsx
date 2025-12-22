e8dc21aa44cae9fca1b539c78918a9c30f02c672
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-12-22 11:23:55 +0800

    style: adjust the button style

--- src/shared/components/buttons/BackButton.tsx
+++ src/shared/components/buttons/BackButton.tsx
@@ -1,10 +1,13 @@
 import { Button } from "../ui/button"
 import { ChevronLeft } from "lucide-react"
+import Link from "next/link"
 
 export default function BackButton(){
     return(
         <Button variant="ghost" className="text-sm cursor-pointer">
-          <ChevronLeft /> Back to Home
+          <Link href={'/'} className="flex items-center">
+            <ChevronLeft className="mr-1"/> Back to Home
+          </Link>
         </Button>
     )
 }
