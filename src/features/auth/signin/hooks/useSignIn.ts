d27dc5c51ed4aa9190fa3c9b6c2257696ce1a39d
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-12-26 09:46:27 +0800

    fix(auth): change the try catch logic

--- src/features/auth/signin/hooks/useSignIn.ts
+++ src/features/auth/signin/hooks/useSignIn.ts
@@ -2,35 +2,49 @@
 
 import { signIn } from "next-auth/react";
 import { useState } from "react";
+import { publicApi } from "@/shared/lib/axios";
 
 export function useSignIn() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   const login = async (email: string, password: string) => {
+
     setLoading(true);
     setError(null);
 
-    try {
-      const result = await signIn("credentials", {
+    try{
+      const res = await publicApi.post('/api/auth', {
         email,
         password,
-        redirect: false,
-      });
+      })
 
-      if (result?.error) {
-        setError(result.error);
-        return false;
+      if(res.status === 200) {
+          await signIn("credentials", {
+          email,
+          password,
+          redirect: false
+        })
       }
 
-      return true;
-    } catch (err) {
-      setError("Something went wrong");
+
+      
+
+       return true;
+    }
+
+    catch(error: any){
+      if (error.response) {
+        setError(error.response.data?.error ?? "Sign in failed");
+      } else {
+        setError("Network error");
+      }
       return false;
     } finally {
       setLoading(false);
     }
-  };
+
+    }
 
   return { login, loading, error };
 }
