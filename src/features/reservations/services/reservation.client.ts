804b080c3fd8f28b5d7fe22ab90f3c13903378fe
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-01-08 00:45:34 +0800

    fix: update the endpoint

--- src/features/reservations/services/reservation.client.ts
+++ src/features/reservations/services/reservation.client.ts
@@ -2,15 +2,23 @@ import { api } from "@/shared/lib/axios";
 import { ReservationFormInput } from "../types/ReservationForm";
 
 export const ReservationClientServices = {
+  async createReservation(data: ReservationFormInput) {
+    const response = await api.post("/api/reserve", data);
+    return response.data;
+  },
 
-    async createReservation( data: ReservationFormInput ) {
-        const response = await api.post<ReservationFormInput>("/api/reservation", data);
-        return response.data;
-    },
+  async getAllReservations() {
+    const response = await api.get("/api/reserve");
+    return response.data.reservations;
+  },
 
-    async getAllReservations(): Promise<ReservationFormInput[]> {
-        const response = await api.get("/api/reservation");
-        return response.data.reservations;
-    }
-    
-}
+  async getReservationById(id: string) {
+    const response = await api.get(`/api/reserve/${id}`);
+    return response.data.reservation;
+  },
+
+  async deleteReservation(id: string) {
+    const response = await api.delete(`/api/reserve/${id}`);
+    return response.data;
+  }
+};
