b891dd129457bf3491cc2d9b726adddfa9afbc08
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-01-07 20:25:36 +0800

    chore: prisma migrations

---
+++ prisma/migrations/20251230142312_add_makeup_look_to_reservation/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Reservation" ADD COLUMN     "makeupLook" TEXT NOT NULL DEFAULT 'Not specified';
---
+++ prisma/migrations/20251230144640_remove_makeup_look/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `makeupLook` on the `Reservation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Reservation" DROP COLUMN "makeupLook";
