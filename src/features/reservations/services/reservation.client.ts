7a04c13dfc5ee2ff6199cbfabbc61b1c080952f7
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-01-08 17:33:36 +0800

    fix(api): change the api endpoint

--- src/features/reservations/services/reservation.client.ts
+++ src/features/reservations/services/reservation.client.ts
@@ -3,22 +3,22 @@ import { ReservationFormInput } from "../types/ReservationForm";
 
 export const ReservationClientServices = {
   async createReservation(data: ReservationFormInput) {
-    const response = await api.post("/api/reserve", data);
+    const response = await api.post("/api/reservation", data);
     return response.data;
   },
 
   async getAllReservations() {
-    const response = await api.get("/api/reserve");
+    const response = await api.get("/api/reservation");
     return response.data.reservations;
   },
 
   async getReservationById(id: string) {
-    const response = await api.get(`/api/reserve/${id}`);
+    const response = await api.get(`/api/reservation/${id}`);
     return response.data.reservation;
   },
 
   async deleteReservation(id: string) {
-    const response = await api.delete(`/api/reserve/${id}`);
+    const response = await api.delete(`/api/reservation/${id}`);
     return response.data;
   }
 };
